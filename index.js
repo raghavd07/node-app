8d0edec0e9d31c4ceda9391233c54d0b17c836e4
raghavd07 <d.sairaghav07@gmail.com>
2026-03-14 16:06:38 +0530

    3rd comimt

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import productRouter from './routes/productRoute.js'
 import dbConnect from './config/db.js'
+
 dotenv.config()
 const PORT=process.env.PORT
 const app = express()
 app.set("view engine","ejs")
 app.set("views","views") //views of this project are saved in the views folder
+app.use(express.urlencoded({extended:true}))
 const startServer=async()=>{
     await dbConnect()
     app.listen(PORT,()=>{
